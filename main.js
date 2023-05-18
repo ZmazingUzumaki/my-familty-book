93c09915cd9230e335d41fb43c08a272f4a3eff1
ZmazingUzumaki <122739235+ZmazingUzumaki@users.noreply.github.com>
2023-05-18 19:06:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var images = ["","", "" ,];
+var names = ["Papa","Mom", "parleen",];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 3
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    var updatedImage = images[i];
+    var updatedName = names[i];
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
